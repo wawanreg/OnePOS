b6dcb5d0207a41077c58bd5b795fdc84741f6158
bernadinus <bernardinus.realino@travelmadezy.com>
2017-02-06 02:39:10 +0700

    first push

---
+++ OnePOS/Startup.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.Owin;
+using Owin;
+
+[assembly: OwinStartupAttribute(typeof(OnePOS.Startup))]
+namespace OnePOS
+{
+    public partial class Startup
+    {
+        public void Configuration(IAppBuilder app)
+        {
+            ConfigureAuth(app);
+        }
+    }
+}
