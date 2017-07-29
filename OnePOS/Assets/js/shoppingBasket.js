2061e635887f47dfda8e9b39ecb82889cd117db4
wawanreg <wawanreg@gmail.com>
2017-07-29 17:58:28 +0700

    commit for shoppingBasket

--- OnePOS/Assets/js/shoppingBasket.js
+++ OnePOS/Assets/js/shoppingBasket.js
@@ -1,4 +1,7 @@
 var shoppingCollection = function (classBelowWarning,mainJsonUrl) {
+
+    
+
     var checkWarning = function (isWarning) {
         if (isWarning)
             $(classBelowWarning).css('margin-top', '10px');
@@ -6,11 +9,12 @@ var shoppingCollection = function (classBelowWarning,mainJsonUrl) {
             $(classBelowWarning).css('margin-top', '');
     }
 
-    var app = angular.module('app', ['ngResource', 'angularUtils.directives.dirPagination']);
+    var app = angular.module('app', ['ngResource', 'angularUtils.directives.dirPagination', 'ui.bootstrap']);
 
 	app.controller('POSController',['$scope', '$http', function ($scope,$http) {
-			$scope.itemColletions = [];
-			
+			$scope.itemCollections = [];
+			$scope.tempItemColls = [];
+
 			$scope.primaryUrl = mainJsonUrl;
 			$scope.init = function() {
 			    $http({
@@ -19,8 +23,21 @@ var shoppingCollection = function (classBelowWarning,mainJsonUrl) {
 			    }).then(function (data, status) {
 			        var collectJson = JSON.parse(data.data.datajson);
 			        for (var i = 0; i < collectJson.length; i++) {
-			            $scope.itemColletions.push(collectJson[i]);
+			            
+			            $scope.itemCollections.push(collectJson[i]);
+
+			            var cloneItem = {
+                            id : i+1,
+			                cloneId: collectJson[i].ItemId,
+			                clonePrice: collectJson[i].ItemPrice,
+			                cloneName: collectJson[i].ItemName,
+			                cloneStock: collectJson[i].TotalStock
+			            }
+
+			            $scope.tempItemColls.push(cloneItem);
 			        }
+			        console.log($scope.tempItemColls);
+
 			    }, function error() {
 
 			    });
@@ -35,9 +52,9 @@ var shoppingCollection = function (classBelowWarning,mainJsonUrl) {
 	            $('#messageLoader').hide();
 	            checkWarning(false);
 
-	            for (var i = 0; i < $scope.itemColletions.length; i++) {
-	                if ($scope.itemColletions[i].ItemId == itemId) {
-	                    var tempItem = $scope.itemColletions[i];
+	            for (var i = 0; i < $scope.itemCollections.length; i++) {
+	                if ($scope.itemCollections[i].ItemId == itemId) {
+	                    var tempItem = $scope.itemCollections[i];
 
 	                    if ($scope.order.length > 0) {
 	                        for (var j = 0; j < $scope.order.length; j++) {
@@ -45,6 +62,8 @@ var shoppingCollection = function (classBelowWarning,mainJsonUrl) {
 	                                
 	                                if (tempItem.TotalStock > 0) {
 	                                    $scope.order[j].qty += 1;
+	                                    $scope.order[j].totalPrice += tempItem.ItemPrice;
+
 	                                    nihil = false;
 	                                    tempItem.TotalStock--;
 	                                    break;
@@ -64,7 +83,8 @@ var shoppingCollection = function (classBelowWarning,mainJsonUrl) {
 	                                    id: tempItem.ItemId,
 	                                    name: tempItem.ItemName,
 	                                    qty: 1,
-	                                    price: tempItem.ItemPrice
+	                                    price: tempItem.ItemPrice,
+	                                    totalPrice: tempItem.ItemPrice
 	                                };
 	                                tempItem.TotalStock--;
 	                                $scope.order.push(orderItem);
@@ -79,9 +99,11 @@ var shoppingCollection = function (classBelowWarning,mainJsonUrl) {
 	                            id: tempItem.ItemId,
 	                            name: tempItem.ItemName,
 	                            qty: 1,
-	                            price: tempItem.ItemPrice
+	                            price: tempItem.ItemPrice,
+	                            totalPrice: tempItem.ItemPrice
 	                        };
 	                        tempItem.TotalStock--;
+	                        console.log($scope.tempItemColls);
 	                        $scope.order.push(orderItem);
 	                    }
                     }
@@ -89,31 +111,73 @@ var shoppingCollection = function (classBelowWarning,mainJsonUrl) {
 	            }
 	        };
 			
+	        $scope.deleteItem = function (index,itemId,itemQty) {
+	            
+	            for (var i = 0; i < $scope.itemCollections.length; i++) {
+	                if ($scope.itemCollections[i].ItemId == itemId) {
+	                    $scope.itemCollections[i].TotalStock += itemQty;
+	                }
+	            }
+	            $scope.order.splice(index, 1);
+	        };
+		
+	        $scope.editItem = function (itemId, itemQty) {
+	            
+
+	            for (var i = 0; i < $scope.itemCollections.length; i++) {
+	                if ($scope.itemCollections[i].ItemId == itemId) {
+	                    var tempColletion = $scope.itemCollections[i];
+	                    var tempItemCol = $scope.tempItemColls[i];
+
+	                    for (var j = 0; j < $scope.order.length; j++) {
+	                        if ($scope.order[j].id == itemId) {
+	                            
+
+	                            if (tempColletion.TotalStock >= itemQty) {
+	                                tempColletion.TotalStock = tempItemCol.TotalStock - itemQty;
+                                    $scope.order[j].qty = itemQty;
+	                                $scope.order[j].totalPrice = $scope.order[i].price * itemQty;
+
+	                            } else {
+	                                $scope.order[j].qty = tempColletion.TotalStock;
+	                               
+
+	                            }
+	                            
+	                        }
+
+	                    }
+                    }
+
+	                
+                }
+	            
+
+	        };
+
 			
+			 $scope.getSum = function() {
+			   var i = 0,
+				 sum = 0;
+			   for(; i < $scope.order.length; i++) {
+			       sum += parseInt($scope.order[i].totalPrice);
+			       
+			   }
+			   return sum;
+			 };
 			
-			
-			// $scope.getSum = function() {
-			  // var i = 0,
-				// sum = 0;
-			  // for(; i < $scope.order.length; i++) {
-				// sum += parseInt($scope.order[i].item.price, 10);
-			  // }
-			  // return sum;
-			// };
-			
-			// $scope.deleteItem = function(index) {
-			  // $scope.order.splice(index,1);
-			// };
-			
-			// $scope.checkout = function(index) {
-			  // alert("Order total: $" + $scope.getSum() + "\n\nPayment received. Thanks.");
-			// };
-			
-			// $scope.clearOrder = function() {
-			  // $scope.order = [];
-			// };
+	        $scope.clearOrder = function() {
+	            $scope.order = [];
+	        };	
 		}
 	]);
+
+	//app.directive('bsPopover', function () {
+	//    return function (scope, element, attrs) {
+	//        console.log("trigger");
+	//        element.find("a[data-toggle=mainTest]").popover();
+	//    };
+	//});
 }
 
 
